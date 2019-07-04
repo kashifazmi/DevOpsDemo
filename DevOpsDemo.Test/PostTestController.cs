fa3d8b1059ad58ab8510781ac4ac395a2477c618
mohd.kashif <mohd.kashif.ext@siemens.com>
2019-07-04 19:10:52 +0530

    Add project files.

---
+++ DevOpsDemo.Test/PostTestController.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DevOpsDemo.Test
+{
+    public class PostTestController
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
